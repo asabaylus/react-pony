edd36ae91ebf68a210ef4213624794d65c8e94eb
Asa Baylus <asa@baylus.com>
2018-01-04 23:31:56 -0500

    use giphy for random pony

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-pony'
+import Pony from 'react-pony'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Pony apiKey='xe8c740Lt4F7dDcsFCIw6HkpJzxmCFKJ' />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,22 +4,47 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import 'whatwg-fetch'
 
 import './styles.css'
 
 export default class Pony extends Component {
   static propTypes = {
-    text: PropTypes.string
+    apiKey: PropTypes.string.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      myPony: {},
+    };
+  }  
+  
+  componentDidMount() {
+     const randInt = Math.floor(Math.random() * 25) + 1;
+     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.props.apiKey}&q=my+little+pony&limit=25&offset=0&rating=Y&lang=en`)
+      .then(response => response.json())
+      .then(response => this.setState({ myPony: response.data[ randInt ] })
+    )
   }
 
   render() {
     const {
-      text
-    } = this.props
-
+      myPony
+    } = this.state
+    
     return (
-      <div className='pony'>
-        <img src='https://media.giphy.com/media/EC5NlYpSCYU8M/giphy.gif'/>
+      <div className="pony">
+        <div>
+          <iframe src={ myPony.embed_url } 
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen>
+          </iframe>
+        </div>
       </div>
     )
   }
