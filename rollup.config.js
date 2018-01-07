6f701c0dd3362842bc75ab06207cfa7efc4ba440
Asa Baylus <asa@baylus.com>
2018-01-07 14:49:58 -0500

    remove iife and cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
 import conditional from 'rollup-plugin-conditional';
 import stripPropTypes from 'rollup-plugin-strip-prop-types';
-import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es';
-import gzip from 'rollup-plugin-gzip';
 
 // Test for Production
 const isProduction = process.env.NODE_ENV === "production";
@@ -17,31 +14,20 @@ export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.js',
-      name: 'Pony',
-      format: 'iife'
-    },
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
       file: 'dist/index.umd.js',
       name: 'Pony',
       format: 'umd',
       sourcemap: true
     },
     {
-      file: 'dist/index.es.js',
+      file: 'lib/index.es.js',
       format: 'es',
       sourcemap: true
     }
   ],
   external: [
     'react',
-    'react-dom',
-    'prop-types'
+    'react-dom'
   ],
   plugins: [
     postcss({}),
@@ -52,8 +38,6 @@ export default {
     commonjs(),
     conditional(isProduction, [
       stripPropTypes(), // save ~35kb remove propTypes
-      // uglify({}, minify),
-      // gzip()
     ])
   ]
 }
